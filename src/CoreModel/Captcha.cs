903d993cb41801db4bc77d20aa1a3f5fc5216d95
GomaskoDP <GomaskoDP@402D-7>
2023-02-16 16:09:15 +0700

    add CoreModel (start Test Captch)

---
+++ src/CoreModel/Captcha.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataModels.Helpers;
+
+namespace CoreModel
+{
+    public static class Captcha
+    {
+        public static (string HashCode, byte[] Image) GenerateCaptchaInfo()
+        {
+            var code = new Random().Next(100000, 1000000).ToString();
+            var hash = HashHundle.GetHashString(code);
+            var image = (new CaptchaGen.SkiaSharp.CaptchaGenerator()).GenerateImageAsByteArray(code);
+            return (hash, image);
+        }
+    }
+}
---
+++ src/DataModels/Helpers/HashHundle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+
+namespace DataModels.Helpers
+
+{
+    public static class HashHundle
+    {
+        public static string GetHashString(string code)
+        {
+            if (string.IsNullOrWhiteSpace(code)) throw new ArgumentException();
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(code));
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
